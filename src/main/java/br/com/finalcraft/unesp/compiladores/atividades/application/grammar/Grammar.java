176ab374b02d421f21d36dc2489806715ead6372
Petrus <petrus_ant@hotmail.com>
2019-09-17 22:49:16 -0300

    Adicionado algumas regras para testar.

--- src/main/java/br/com/finalcraft/unesp/compiladores/atividades/application/grammar/Grammar.java
+++ src/main/java/br/com/finalcraft/unesp/compiladores/atividades/application/grammar/Grammar.java
@@ -3,7 +3,6 @@ package br.com.finalcraft.unesp.compiladores.atividades.application.grammar;
 import br.com.finalcraft.unesp.compiladores.atividades.application.grammar.data.Derivacao;
 import br.com.finalcraft.unesp.compiladores.atividades.application.grammar.data.NaoTerminal;
 import br.com.finalcraft.unesp.compiladores.atividades.application.grammar.data.Terminal;
-import br.com.finalcraft.unesp.compiladores.atividades.application.lexema.LexemaType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,12 +33,42 @@ public class Grammar {
         return origem;
     }
 
+    // -----------------------------------------------------------------------------------------------------------------
+    // Program
+    // -----------------------------------------------------------------------------------------------------------------
     public static final Grammar PROGRAM = new Grammar(NaoTerminal.PROGRAM)
             .addDerivacao(Derivacao.valueOf(new Terminal("program"), new Terminal("identificador"), new Terminal(";"), NaoTerminal.BLOCO, new Terminal(".")));
 
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Bloco
+    // -----------------------------------------------------------------------------------------------------------------
     public static final Grammar BLOCO = new Grammar(NaoTerminal.BLOCO)
-            .addDerivacao(Derivacao.valueOf(new Terminal("if"), new Terminal("(")))
-            .addDerivacao(Derivacao.valueOf(new Terminal("identificador"), new Terminal(";")));
+            .addDerivacao(Derivacao.valueOf(NaoTerminal.PARTE_DE_DECLARACAO_DE_VARIAVEIS,NaoTerminal.PARTE_DE_DECLARACAO_DE_SUB_ROTINAS,NaoTerminal.COMANDO_COMPOSTO));
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Parte de Declaração de Variáveis
+    // -----------------------------------------------------------------------------------------------------------------
+    public static final Grammar PARTE_DE_DECLARACAO_DE_VARIAVEIS = new Grammar(NaoTerminal.PARTE_DE_DECLARACAO_DE_VARIAVEIS)
+            .addDerivacao(Derivacao.valueOf(NaoTerminal.DECLARACAO_DE_VARIAVEIS, new Terminal(";"),NaoTerminal.PARTE_DE_DECLARACAO_DE_VARIAVEIS))
+            .addDerivacao(Derivacao.valueOf());//Derivação vazia --> Optional
+
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Declaração de Variáveis
+    // -----------------------------------------------------------------------------------------------------------------
+    public static final Grammar DECLARACAO_DE_VARIAVEIS = new Grammar(NaoTerminal.DECLARACAO_DE_VARIAVEIS)
+            .addDerivacao(Derivacao.valueOf(NaoTerminal.TIPO,NaoTerminal.TIPO, new Terminal(";"),NaoTerminal.PARTE_DE_DECLARACAO_DE_VARIAVEIS))
+            .addDerivacao(Derivacao.valueOf());//Derivação vazia --> Optional
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Parte de Declaração de SubRotinas
+    // -----------------------------------------------------------------------------------------------------------------
+    public static final Grammar PARTE_DE_DECLARACAO_DE_SUB_ROTINAS = new Grammar(NaoTerminal.PARTE_DE_DECLARACAO_DE_SUB_ROTINAS)
+            .addDerivacao(Derivacao.valueOf(NaoTerminal.DECLARACAO_DE_PROCEDIMENTOS,NaoTerminal.PARTE_DE_DECLARACAO_DE_SUB_ROTINAS))
+            .addDerivacao(Derivacao.valueOf());//Derivação vazia --> Optional
+
 
 
 }
--- src/main/java/br/com/finalcraft/unesp/compiladores/atividades/application/grammar/data/NaoTerminal.java
+++ src/main/java/br/com/finalcraft/unesp/compiladores/atividades/application/grammar/data/NaoTerminal.java
@@ -25,6 +25,21 @@ public class NaoTerminal implements Derivacao{
     public static final NaoTerminal PROGRAM = new NaoTerminal("<program>");
     public static final NaoTerminal BLOCO = new NaoTerminal("<bloco>");
 
+    public static final NaoTerminal PARTE_DE_DECLARACAO_DE_VARIAVEIS = new NaoTerminal("<parte de declarações de variáveis>");
+    public static final NaoTerminal PARTE_DE_DECLARACAO_DE_SUB_ROTINAS = new NaoTerminal("<parte de declarações de sub-rotinas>");
+
+
+    public static final NaoTerminal DECLARACAO_DE_PROCEDIMENTOS = new NaoTerminal("<declaração de procedimento>");
+
+    public static final NaoTerminal DECLARACAO_DE_VARIAVEIS = new NaoTerminal("<declaração de variáveis>");
+
+
+    public static final NaoTerminal COMANDO_COMPOSTO = new NaoTerminal("<comando composto>");
+
+
+    public static final NaoTerminal TIPO = new NaoTerminal("<tipo>");
+    public static final NaoTerminal LISTA_DE_IDENTIFICADORES = new NaoTerminal("<lista de identificadores>");
+
 
     @Override
     public String toString() {
